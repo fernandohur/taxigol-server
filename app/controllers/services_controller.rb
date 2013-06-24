3e4980f54898855a3e35924a9b0ae6daf3358a3a
fernandohur <fernandohur@gmail.com>
2013-06-24 17:37:12 -0500

    added comments and improved code readability

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,11 @@
 class ServicesController < ApplicationController
+
   # GET /services
   # GET /services.json
   #params :taxi_id optionally receives a taxi id in which case returns all Services who's taxi_id matches the parameter
   def index
     taxi_id = params[:taxi_id]
-    if taxi_id then
+    if taxi_id
       @services = Service.get_pending_or_confirmed(taxi_id)
     else
       @services = Service.all
@@ -52,9 +53,9 @@ class ServicesController < ApplicationController
     address = params[:address]
     tip = params[:tip]
     if !tip
-    	@service = Service.construct(verification_code,address,latitude,longitude)
-    else
     	@service = Service.construct(verification_code, address, latitude,longitude, tip)
+    else
+    	@service = Service.construct(verification_code,address,latitude,longitude)
     end
     respond_to do |format|
       if @service.save
@@ -80,83 +81,27 @@ class ServicesController < ApplicationController
 
   end
 
-    # DELETE /services/1
-    # DELETE /services/1.json
-    def destroy
-      @service = Service.find(params[:id])
-      @service.destroy
+	# DELETE /services/1
+  # DELETE /services/1.json
+	def destroy
+		@service = Service.find(params[:id])
+		@service.destroy
 
-      respond_to do |format|
-        format.html { redirect_to services_url }
-        format.json { head :no_content }
-      end
-    end
+		respond_to do |format|
+			format.html { redirect_to services_url }
+			format.json { head :no_content }
+		end
+	end
 
-    def reset
-      x = Service.delete_all
-      respond_to do |format|
-        format.json {render json: x}
-      end
-    end
+	# POST /services/reset
+	def reset
+		num_deleted = Service.delete_all
+		respond_to do |format|
+			format.json {render json: num_deleted}
+		end
+	end
 
-    def pie_graph
-      g = Gruff::Pie.new("600x500")
-      g.hide_title = true
-      g.title="Cumplimiento de los taxis"
-      g.theme = graph_theme
 
-      x = Service.get_states_count
-      puts(x)
-      x.each_with_index do |f, i|
-        somevar = x[i]
-        g.data(somevar['state'], somevar['TotalCount'])
-      end
-
-      headers["Content-Type"] = "image/png"
-      send_data(g.to_blob, :type => "image/png", :disposition => "inline")
-    end
-
-  def pie_graph_for_taxi (taxi_id)
-    g = Gruff::Pie.new("600x500")
-    g.hide_title = true
-    g.title="Cumplimiento del taxista"
-    g.theme = graph_theme
-
-    x = Service.get_taxi_states_count(taxi_id)
-    puts(x)
-    x.each_with_index do |f, i|
-      somevar = x[i]
-      g.data(somevar['state'], somevar['TotalCount'])
-    end
-
-    headers["Content-Type"] = "image/png"
-    send_data(g.to_blob, :type => "image/png", :disposition => "inline")
-  end
-
-  def bar_graph
-    g = Gruff::Bar.new("600x500")
-    g.hide_title = true
-    g.title="Cumplimiento del taxista"
-    g.theme = graph_theme
-
-    x = Service.get_states_count
-    puts(x)
-    x.each_with_index do |f, i|
-      somevar = x[i]
-      g.data(somevar['state'], somevar['TotalCount'])
-    end
-
-    headers["Content-Type"] = "image/png"
-    send_data(g.to_blob, :type => "image/png", :disposition => "inline")
-  end
-
-    def graph_theme
-      {
-          :colors => ["#DB2626", "#6A6ADB", "#64D564", "#F727F7", "#EBEB20", "#303030", "#12ABAD", "#808080", "#B7580B", "#316211"],
-          :marker_color => "#AAAAAA",
-          :background_colors => ["#FFFFFF", "#FFFFFF"]
-      }
-    end
 
 
 end
