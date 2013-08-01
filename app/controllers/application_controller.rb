c22ea194ff99989b19f92031348bf75096ed813f
fernandohur <fernandohur@gmail.com>
2013-07-31 19:05:45 -0500

    minor fixes, improved docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
 
 	after_filter :set_access_control_headers
 
+  #
+  # permit javascript to execute out of origin
+  # 
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
   end
 
+  #
+  # renders an error object with the following format
+  # {
+  #   error:'<name of the error>',
+  #   message:'a descriptive message showing the nature of the error'
+  # }
+  #
   def render_error(e)
 
     json = {:error=>e.class.to_s, :message=>e.message}.to_json
--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -23,22 +23,6 @@ class DriversController < ApplicationController
     end
   end
 
-  # GET /drivers/new
-  # GET /drivers/new.json
-  def new
-    @driver = Driver.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @driver }
-    end
-  end
-
-  # GET /drivers/1/edit
-  def edit
-    @driver = Driver.find(params[:id])
-  end
-
   # POST /drivers
   # POST /drivers.json
   def create
@@ -63,7 +47,8 @@ class DriversController < ApplicationController
 
   #
   # POST /drivers/auth.json
-  #
+  # @param :cedula || :username 
+  # @param :password
   def auth
   	if params[:cedula]
     	username = params[:cedula]
@@ -77,26 +62,22 @@ class DriversController < ApplicationController
     else
       render_message('wrong credentials, sry :)')
     end
-
   end
 
-  #
-  # GET /drivers/get_drivers.json
   # returns all the drivers that match a given taxi id
-  #
+  # @path GET /drivers/get_drivers.json
+  # @param :taxi_id 
   def get_drivers
-
     taxi_id = params[:taxi_id]
     @drivers = Driver.get_by_taxi_id(taxi_id)
     render_as_json @drivers
-
   end
 
+  # updates a driver
   # PUT /drivers/1
   # PUT /drivers/1.json
   def update
     @driver = Driver.find(params[:id])
-
     respond_to do |format|
       if @driver.update_attributes(params[:driver])
         format.html { redirect_to @driver, notice: 'Driver was successfully updated.' }
@@ -108,18 +89,7 @@ class DriversController < ApplicationController
     end
   end
 
-  # DELETE /drivers/1
-  # DELETE /drivers/1.json
-  def destroy
-    @driver = Driver.find(params[:id])
-    @driver.destroy
-
-    respond_to do |format|
-      format.html { redirect_to drivers_url }
-      format.json { head :no_content }
-    end
-  end
-
+  # removes all drivers
   # POST /drivers/reset
   def reset
     num_deleted = Driver.delete_all
