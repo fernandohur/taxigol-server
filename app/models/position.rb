710f38c585e4077422f754a3d4367b981626225c
fernandohur <fernandohur@gmail.com>
2013-08-03 23:04:50 -0500

    added docs to position

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,10 @@
+#
+# the Position class represents a taxi's position in space
+# A position is identified by a latitude, longitude and the id of the taxi
+# who created the position.
+# If a taxi's position changes, a new Position object is created, a Position object 
+# is not updated.
+#
 class Position < ActiveRecord::Base
   attr_accessible :latitude, :longitude, :taxi_id
   belongs_to :taxi
