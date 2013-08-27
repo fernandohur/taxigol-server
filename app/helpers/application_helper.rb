c231e3671aa6ea630c49b545ac0486d87678f08e
fernandohur <fernandohur@gmail.com>
2013-08-27 16:22:04 -0500

    added documentation for app helpers and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
 		render :partial=>"common/timeago",:locals=>{:date=>date}
 	end
 
+	#
+	# helper method to pretty print a service's state using the
+	# common/state partial
+	#
 	def render_state_label(service)
 		render :partial=>"common/state",:locals=>{:service=>service}
 	end
