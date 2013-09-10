f1fdd832e480a4d3c31f11349a51f117473d38ad
Carlos E. Duque <carlosduque@Carloss-MacBook-Pro.local>
2013-09-10 16:27:27 -0500

    mensaje de broadcast

--- app/controllers/broadcaster_controller.rb
+++ app/controllers/broadcaster_controller.rb
@@ -21,9 +21,11 @@ class BroadcasterController < ApplicationController
     id_user = params[:user_id]
     message = params[:message]
     User.find(id_user).push_notification(message)
-    rescue ActiveRecord::RecordNotFound
-      respond_with nil
     render_message('broadcast sent successfully')
+    rescue => ex
+      respond_to do |format|
+            format.json { render :json => ex.message, :status => 400 }
+      end
   end
 
 end
